1b93f4eae1cfdfb3b9b99e59811f3e385af734ef
Gerardo Fabian Quispe <gfq0684@gmail.com>
2022-06-29 19:54:08 -0300

    primer commit

---
+++ CartasEspaniolas/Modelo/Baraja.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartasEspaniolas.Modelo
+{
+    internal class Baraja
+    {
+        public List<int> Mazo { get; set; }
+        public List<int> Monton { get; set; }
+
+        public Baraja()
+        {
+            // Creamos el mazo de cartas españolas
+            // del 01 - 10 son las de ORO = ♦.
+            // del 11 - 20 son las de COPA = ♥.
+            // del 21 - 30 son las de ESPADAS = ♠.
+            // del 31 - 40 son las de PALO = ♣.
+            Mazo = new List<int>();
+            for (int i = 1; i <= 40; i++)
+            {
+                Mazo.Add(i);
+            }
+
+            Monton = new List<int>();
+        }
+
+
+        // Mezclamos las cartas.
+        // Cambiamos de posición todas las cartas aleatoriamente.
+        public void Barajar()
+        {
+            if (Mazo.Count() != 0) // Comporbamos que la baraja tenga almenos una carta
+            {
+                Random rand = new Random();
+                for (int i = 0; i < Mazo.Count(); i++)
+                {
+                    int j = rand.Next(0, Mazo.Count());
+                    int aux = Mazo[i];
+                    Mazo[i] = Mazo[j];
+                    Mazo[j] = aux;
+                }
+                Console.WriteLine("\nSe a mezclado la Baraja\n");
+            }
+            else
+                Console.WriteLine("\nNo hay cartas para Barajar...");
+        }
+
+        // Devuelve la siguiente carta que está en la baraja,
+        // cuando no hay más o se haya llegado al final, se indica
+        // al usuario que no hay más cartas.
+        public void SiguienteCarta()
+        {
+            if (Mazo.Count() != 0) // Comporbamos que la baraja tenga almenos una carta
+                Console.WriteLine($"\nSiguiente Carta {Naipe(Mazo[Mazo.Count() - 1])}\n");
+            else
+                Console.WriteLine("\nNo hay más cartas...\n");
+        }
+
+        // Devuelve el número de cartas que aún puede repartir
+        public int CartasDisponibles()
+        {
+            return Mazo.Count();
+        }
+
+        // Dado un número de cartas que nos pidan, le devolveremos ese número de cartas
+        // En caso de que haya menos cartas que las pedidas, no devolveremos nada pero
+        // le indicamos al usuario que no hay cartas suficientes.
+        public void DarCartas(int cantidad)
+        {
+            int cantCartas = Mazo.Count();
+            if (cantidad <= cantCartas) // Comprobamos que la cantidad de cartas pedidas no sea superior a las existentes
+            {
+                for (int i = 0; i < cantidad; i++)
+                {
+                    Console.WriteLine($"Toma el {Naipe(Mazo[cantCartas - i - 1])}");
+                    Monton.Add(Mazo[cantCartas - i - 1]);
+                }
+                RemoverCarta(cantidad);
+            }
+            else
+                Console.WriteLine("\nNo se disponen de la cantidad de cartas solicitadas...\n");
+
+        }
+
+        // Eliminamos de la Baraja las cartas que se han pedido.
+        public void RemoverCarta(int cantidad)
+        {
+            for (int i = 0; i < cantidad; i++)
+            {
+                Mazo.RemoveAt(Mazo.Count() - 1);
+            }
+        }
+
+        // Mostramos aquellas cartas que ya han salido, si no ha salido
+        // ninguna indicárselo al usuario.
+        public void CartasMonton()
+        {
+            if (Monton.Count() != 0) // Comporbamos que el Monton tenga almenos una carta
+            {
+                for (int i = 0; i < Monton.Count(); i++)
+                {
+                    Console.Write($"  {Naipe(Monton[i])}");
+                    if ((i + 1) % 10 == 0) Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("\nAun no a pedido ninguna carta...\n");
+
+            Console.WriteLine();
+        }
+
+        // Muestra todas las cartas hasta el final.
+        // Es decir, si se saca una carta y luego se llama al método,
+        // este no mostrara esa primera carta.
+        public void MostrarBaraja()
+        {
+            Console.WriteLine("\nBaraja:\n");
+            if (Mazo.Count() != 0) // Comporbamos que la baraja tenga almenos una carta
+            {
+                for (int i = 0; i < Mazo.Count; i++)
+                {
+                    Console.Write($"  {Naipe(Mazo[i])}");
+                    if ((i + 1) % 10 == 0) Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("No hay cartas para Mostrar...");
+
+            Console.WriteLine();
+        }
+
+
+        // del 01 - 10 son las de ORO = ♦.
+        // del 11 - 20 son las de COPA = ♥.
+        // del 21 - 30 son las de ESPADAS = ♠.
+        // del 31 - 40 son las de PALO = ♣.
+        public string Naipe(int carta)
+        {
+            if (carta >= 1 && carta <= 10)
+                return ($"{(carta % 10):00}_♦");
+            else if (carta >= 11 && carta <= 20)
+                return ($"{(carta % 10):00}_♥");
+            else if (carta >= 21 && carta <= 30)
+                return ($"{(carta % 10):00}_♠");
+            else
+                return ($"{(carta % 10):00}_♣");
+        }
+    }
+}
---
+++ CartasEspaniolas/Program.cs
@@ -0,0 +1,106 @@
+﻿// Clase 14 Ejercicio - Cartas españolas
+// Gerardo Fabián Quispe
+
+/*  Ejercicio de cartas españolas orientado a objetos:
+    Cada carta tiene un número entre 1 y 12 (el 8 y el 9 no los incluimos) y 
+    un palo (espadas, bastos, oros y copas).
+    La baraja estará compuesta por un conjunto de cartas, 40 exactamente.
+
+    Las operaciones que podrá realizar la baraja son:
+    Barajar():
+    Cambia de posición todas las cartas aleatoriamente.
+
+    SiguienteCarta():
+    Devuelve la siguiente carta que está en la baraja, cuando no haya más 
+    o se haya llegado al final, se indica al usuario que no hay más cartas.
+
+    CartasDisponibles():
+    Indica el número de cartas que aún puede repartir.
+
+    DarCartas(int cantidad):
+    Dado un número de cartas que nos pidan, le devolveremos ese número de
+    cartas (piensa que puedes devolver). En caso de que haya menos cartas 
+    que las pedidas, no devolveremos nada pero debemos indicárselo al usuario.
+
+    CartasMonton():
+    Mostramos aquellas cartas que ya han salido, si no ha salido ninguna 
+    indicárselo al usuario.
+
+    MostrarBaraja():
+    Muestra todas las cartas hasta el final. Es decir, si se saca una carta 
+    y luego se llama al método, este no mostrara esa primera carta.
+
+    Escribir un programa que dentro de un bucle vaya mostrando las opciones
+    que querramos, por ejemplo:
+
+        1 - Barajar
+        2 - Mostrar siguiente carta
+        3 - Mostrar cartas disponibles
+        4 - Dar cartas
+        5 - Mostrar cartas del monton
+        6 - Mostrar baraja
+        7 - Salir
+ */
+
+//Console.WriteLine("hola");
+using CartasEspaniolas.Modelo;
+
+Baraja Mazzo = new Baraja();
+
+int opc = 0;
+do
+{
+    Console.WriteLine("############### CARTAS ESPAÑOLAS ###############");
+    Console.WriteLine("  1.  Barajar.");
+    Console.WriteLine("  2.  Mostrar siguiente carta.");
+    Console.WriteLine("  3.  Mostrar cartas disponibles.");
+    Console.WriteLine("  4.  Dar cartas.");
+    Console.WriteLine("  5.  Mostrar cartas del monton.");
+    Console.WriteLine("  6.  Mostrar baraja.");
+    Console.WriteLine("  7.  Salir.");
+    Console.Write("  Eliga una opcion: ");
+    opc = int.Parse(Console.ReadLine());
+
+    Console.Clear();
+
+    switch (opc)
+    {
+        case 1:
+            Mazzo.Barajar();
+            Console.WriteLine();
+            break;
+        case 2:
+            Mazzo.SiguienteCarta();
+            Console.WriteLine();
+            break;
+        case 3:
+            Console.WriteLine($"Cartas dispnibles: {Mazzo.CartasDisponibles()}");
+            Console.WriteLine();
+            break;
+        case 4:
+            Console.Write("Cuantas Cartas quiere: ");
+            int nroCartas = int.Parse(Console.ReadLine());
+            Mazzo.DarCartas(nroCartas);
+            Console.WriteLine();
+            break;
+        case 5:
+            Console.WriteLine("\nCartas del Monton:");
+            Mazzo.CartasMonton();
+            Console.WriteLine();
+            break;
+        case 6:
+            Mazzo.MostrarBaraja();
+            Console.WriteLine();
+            break;
+        case 7:
+            Console.WriteLine("\n  Hasta pronto...\n");
+            break;
+        default:
+            Console.WriteLine("Opcion no Disponible...");
+            Console.WriteLine();
+            break;
+    }
+
+} while (opc != 7);
+
+
